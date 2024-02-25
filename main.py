380f515d7e432a4eefa7c07e107ee35466c20344
deepjyoti7147 <deepjyoti.college@gmail.com>
2024-02-25 22:20:21 +0530

    Commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pathlib
 import textwrap
-
+import google.ai.generativelanguage as glm
 import google.generativeai as genai
 
 from IPython.display import display
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get the API key from the .env file
-api_key = os.getenv("API_KEY")
+# api_key = os.getenv("API_KEY")
 
 
 def to_markdown(text):
@@ -23,10 +23,24 @@ def to_markdown(text):
 
 
 
-GOOGLE_API_KEY= api_key
+GOOGLE_API_KEY= os.getenv("API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
 
-
+'''
 for m in genai.list_models():
   if 'generateContent' in m.supported_generation_methods:
     print(m.name)
+'''
+
+model = genai.GenerativeModel('gemini-pro')
+
+prompt1 = input("Enter a prompt: ")
+
+response = genai.generate_text(prompt=prompt1)
+print(response.result)  # cold.
+
+'''response = model.generate_content("What is the meaning of life?")
+response.prompt_feedback
+for chunk in response:
+  print(chunk.text)
+  print("_"*80)'''
